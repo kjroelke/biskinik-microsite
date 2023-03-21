6bde01524287f9f824548eee59ffd13edd59a6bf
KJ Roelke <kj.roelke@gmail.com>
2023-03-21 16:21:21 -0500

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Biskinik';
+export const SITE_DESCRIPTION =
+	'The official newspaper of the Choctaw Nation of Oklahoma';
